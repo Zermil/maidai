5fca96c0ab051610bf8eb37fd2bee07c37f84de3
Aiden <marcin.madry@interia.com>
2023-12-07 17:06:25 +0100

    Highlight notes sent from MIDI controller

--- code/main.cpp
+++ code/main.cpp
@@ -8,6 +8,7 @@
 
 #include "./windows_platform.h"
 #define NOTE_ON 0x90
+#define NOTE_OFF 0x80
 
 #define internal static
 #define global static
@@ -18,7 +19,18 @@
 #define MIN_HEIGHT 700
 #define FPS 60
 
-#define MIDI_LEN 22
+#define MIDI_LEN 22 // @Note: Length in white keys
+#define MIDI_FULL_LEN 37
+
+struct Note {
+    Rectangle rect;
+    int note_number;
+};
+
+global bool g_highlighted_notes[MIDI_FULL_LEN] = {0};
+global const char *g_log_message = 0;
+global bool g_midi_device_started = false;
+global MMRESULT g_open_device_result = MMSYSERR_BADDEVICEID;
 
 internal void draw_text_centered(const char *text, int x, int y, int font_size)
 {
@@ -28,10 +40,10 @@ internal void draw_text_centered(const char *text, int x, int y, int font_size)
 
 // @Note: Yes this is linear, we have 15 black keys/22 white keys, this is not a performance bottle-neck as CPUs are fast.
 // Feel free to change it if you have other, better ideas though.
-internal bool collides_with_keys(Rectangle *keys, size_t size)
+internal bool collides_with_keys(Note *keys, size_t size)
 {
     for (size_t i = 0; i < size; ++i) {
-        if (CheckCollisionPointRec(GetMousePosition(), keys[i])) {
+        if (CheckCollisionPointRec(GetMousePosition(), keys[i].rect)) {
             return(true);
         }
     }
@@ -59,16 +71,19 @@ internal void render_keyboard(Rectangle rect, int key_width, int key_padding)
     // handling, because rectangles overlap each-other
     // I wanted to solve this problem with z-index or something similar
     // but raylib (afaik) doesn't provide anything like that (nor layers etc.)
-    Rectangle black_keys[MIDI_LEN - 1] = {0};
-    Rectangle white_keys[MIDI_LEN] = {0};
+    Note black_keys[MIDI_LEN - 1] = {0};
+    Note white_keys[MIDI_LEN] = {0};
     
-    for (int i = 0, j = 0; i < MIDI_LEN; ++i) {
-        white_keys[i] = white_key;
-
+    for (int i = 0, note_number = 0; i < MIDI_LEN; ++i) {
+        white_keys[i].rect = white_key;
+        white_keys[i].note_number = note_number;
+        note_number += 1;
+        
         // @Robustness: Change this, it's just ugly.
         if (i != MIDI_LEN - 1 && (i % 7 != 6 && i % 7 != 2)) {
-            black_keys[j] = black_key;
-            j += 1;
+            black_keys[i].rect = black_key;
+            black_keys[i].note_number = note_number;
+            note_number += 1;
         }
         
         white_key.x += key_width + key_padding;
@@ -78,29 +93,31 @@ internal void render_keyboard(Rectangle rect, int key_width, int key_padding)
     for (int i = 0; i < MIDI_LEN; ++i) {
         bool black_key_collision = collides_with_keys(black_keys, MIDI_LEN - 1);
         
-        if (CheckCollisionPointRec(GetMousePosition(), white_keys[i]) && !black_key_collision) {
-            DrawRectangleRec(white_keys[i], RED);
+        if (CheckCollisionPointRec(GetMousePosition(), white_keys[i].rect) && !black_key_collision) {
+            DrawRectangleRec(white_keys[i].rect, RED);
 
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                printf("[WHITE_KEY_%d]\n", i);
+                printf("[WHITE_KEY_%d]\n", white_keys[i].note_number);
             }
         } else {
-            DrawRectangleRec(white_keys[i], WHITE);
+            bool highlight = g_highlighted_notes[white_keys[i].note_number];
+            DrawRectangleRec(white_keys[i].rect, highlight ? GREEN : WHITE);
         }
     }
 
     for (int i = 0; i < MIDI_LEN - 1; ++i) {
         bool white_key_collision = collides_with_keys(white_keys, MIDI_LEN);
-        bool collision = CheckCollisionPointRec(GetMousePosition(), black_keys[i]);
+        bool collision = CheckCollisionPointRec(GetMousePosition(), black_keys[i].rect);
 
         if (collision || (collision && white_key_collision)) {
-            DrawRectangleRec(black_keys[i], RED);
+            DrawRectangleRec(black_keys[i].rect, RED);
 
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                printf("[BLACK_KEY_%d]\n", i);
+                printf("[BLACK_KEY_%d]\n", black_keys[i].note_number);
             }
         } else {
-            DrawRectangleRec(black_keys[i], BLACK);
+            bool highlight = g_highlighted_notes[black_keys[i].note_number];
+            DrawRectangleRec(black_keys[i].rect, highlight ? GREEN : BLACK);
         }
     }
 }
@@ -137,19 +154,33 @@ internal void render_control_panel(Rectangle rect, int button_padding, int num_b
     }
 }
 
+// @Note: This thing is so poorly document it's like John Microsoft doesn't want us
+// to develop things for their system.
 internal void CALLBACK midi_callback(HMIDIIN handle, UINT msg, DWORD_PTR instance, DWORD_PTR arg0, DWORD_PTR arg1)
-{
+{    
     UNUSED(arg1);
     UNUSED(handle);
     UNUSED(instance);
 
-    if (msg != MIM_DATA) return;
-
-    unsigned int midi_message = arg0 & 0xFF;
-    unsigned int note = (arg0 >> 8) & 0xFF;
-
-    if (midi_message == NOTE_ON) {
-        printf("%u\n", note);
+    // @ToDo: Device won't send MIM_CLOSE message when disconnected
+    
+    if (msg == MIM_DATA) {
+        unsigned int midi_message = arg0 & 0xFF;
+        unsigned int note = (arg0 >> 8) & 0xFF;
+    
+        int index = note - 48;
+        if (midi_message == NOTE_ON) {
+            if (index >= 0 && index < MIDI_FULL_LEN) {
+                g_log_message = "Highlighting note";
+                g_highlighted_notes[index] = true;
+            } else {
+                g_log_message = "Current note is outside the visible range";
+            }
+        } else {
+            if (index >= 0 && index < MIDI_FULL_LEN) {
+                g_highlighted_notes[index] = false;
+            }
+        }
     }
 }
 
@@ -164,7 +195,7 @@ int main(int argc, char **argv)
     SetTargetFPS(FPS);
 
     HMIDIIN handle = 0;
-    MMRESULT open_device_result = midiInOpen(&handle, 0, (DWORD_PTR) midi_callback, 0, CALLBACK_FUNCTION);
+    g_open_device_result = midiInOpen(&handle, 0, (DWORD_PTR) midi_callback, 0, CALLBACK_FUNCTION);
     
     while (!WindowShouldClose()) {
         const int key_width = (int) (GetScreenWidth() * 0.028f);
@@ -192,13 +223,18 @@ int main(int argc, char **argv)
         render_keyboard(keyboard_rect, key_width, key_padding);
         render_control_panel(control_panel_rect, 20, 4);
 
-        if (open_device_result != MMSYSERR_NOERROR) {
-            draw_text_centered("Could not find MIDI device!", (int) text_center.x, (int) text_center.y, 30);
-            open_device_result = midiInOpen(&handle, 0, (DWORD_PTR) midi_callback, 0, CALLBACK_FUNCTION);
-        } else {
-            draw_text_centered("MIDI device connected!", (int) text_center.x, (int) text_center.y, 30);
+        if (g_open_device_result != MMSYSERR_NOERROR) {
+            g_log_message = "Could not find MIDI device";
+            
+            g_open_device_result = midiInOpen(&handle, 0, (DWORD_PTR) midi_callback, 0, CALLBACK_FUNCTION);
+        } else if (g_midi_device_started == false) {
+            g_log_message = "MIDI device connected!";
+            
             midiInStart(handle);
+            g_midi_device_started = true;
         }
+        
+        draw_text_centered(g_log_message, (int) text_center.x, (int) text_center.y, 30);
 
         EndDrawing();
     }

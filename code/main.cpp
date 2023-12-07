3d7fa084fcc0b568dc7063069a0ac378f48c4512
Aiden <marcin.madry@interia.com>
2023-12-07 12:30:18 +0100

    Very dumb but working rect-mouse collision detection

--- code/main.cpp
+++ code/main.cpp
@@ -23,23 +23,82 @@ internal void draw_text_centered(const char *text, int x, int y, int font_size)
     DrawText(text, x - text_width/2, y - font_size/2, font_size, WHITE);
 }
 
+internal bool collides_with_keys(Rectangle *keys, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        if (CheckCollisionPointRec(GetMousePosition(), keys[i])) {
+            return(true);
+        }
+    }
+
+    return(false);
+}
+
 // @Note: By default we render 'regular/extended' ffxiv keyboard, at some point
 // in the future we might generalize it to more/less keys.
 internal void render_keyboard(Rectangle rect, int key_width, int key_padding)
 {
+    Rectangle white_key = {0};
+    white_key.width = (float) key_width;
+    white_key.height = rect.height;
+    white_key.x = 0;
+    white_key.y = rect.y;
+
+    Rectangle black_key = {0};
+    black_key.width = (float) key_width;
+    black_key.height = rect.height/2.0f;
+    black_key.x = (key_width + key_padding)/2.0f;
+    black_key.y = rect.y;
+
+    // @Note: We need to decouple rendering from logic
+    // handling, because rectangles overlap each-other
+    // I wanted to solve this problem with z-index or something similar
+    // but raylib (afaik) doesn't provide anything like that (nor layers etc.)
+    Rectangle black_keys[MIDI_LEN - 1] = {0};
+    Rectangle white_keys[MIDI_LEN] = {0};
+    
+    for (int i = 0, j = 0; i < MIDI_LEN; ++i) {
+        white_keys[i] = white_key;
+
+        // @Robustness: Change this, it's just ugly.
+        if (i != MIDI_LEN - 1 && (i % 7 != 6 && i % 7 != 2)) {
+            black_keys[j] = black_key;
+            j += 1;
+        }
+        
+        white_key.x += key_width + key_padding;
+        black_key.x += key_width + key_padding;
+    }
+
     for (int i = 0; i < MIDI_LEN; ++i) {
-        int key_offset = i*(key_width + key_padding);
-        DrawRectangle(key_offset, (int) rect.y, key_width, (int) rect.height, WHITE);
+        // @Note: Yes this is linear, we have 15 black keys/22 white keys, this is not a performance bottle-neck as CPUs are fast.
+        // Feel free to change it if you have other, better ideas though.
+        bool black_key_collision = collides_with_keys(black_keys, MIDI_LEN - 1);
+        
+        if (CheckCollisionPointRec(GetMousePosition(), white_keys[i]) && !black_key_collision) {
+            DrawRectangleRec(white_keys[i], RED);
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                printf("[WHITE_KEY_%d]\n", i);
+            }
+        } else {
+            DrawRectangleRec(white_keys[i], WHITE);
+        }
     }
 
     for (int i = 0; i < MIDI_LEN - 1; ++i) {
-        // @Robustness: Change this, it's just ugly.
-        if (i % 7 == 6 || i % 7 == 2) continue;
+        bool white_key_collision = collides_with_keys(white_keys, MIDI_LEN);
+        bool collision = CheckCollisionPointRec(GetMousePosition(), black_keys[i]);
 
-        int half_white_width = (key_width + key_padding)/2;
-        int key_offset = half_white_width + i*(key_width + key_padding);
-        
-        DrawRectangle(key_offset, (int) rect.y, key_width, (int) rect.height/2, BLACK);
+        if (collision || (collision && white_key_collision)) {
+            DrawRectangleRec(black_keys[i], RED);
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                printf("[BLACK_KEY_%d]\n", i);
+            }
+        } else {
+            DrawRectangleRec(black_keys[i], BLACK);
+        }
     }
 }
 
@@ -58,8 +117,17 @@ internal void render_control_panel(Rectangle rect, int button_padding, int num_b
     text_center.y = button_rect.y + button_rect.height/2.0f;
 
     for (int i = 0; i < num_buttons; ++i) {
-        DrawRectangleRounded(button_rect, 0.4f, 0, { 50, 50, 50, 255 });
-        draw_text_centered("preset", (int) text_center.x, (int) text_center.y, 22);
+        if (CheckCollisionPointRec(GetMousePosition(), button_rect)) {
+            DrawRectangleRounded(button_rect, 0.4f, 0, { 70, 70, 70, 255 });
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                printf("[PRESET_BUTTON]\n");
+            }
+        } else {
+            DrawRectangleRounded(button_rect, 0.4f, 0, { 50, 50, 50, 255 });
+        }
+        
+        draw_text_centered("preset", (int) text_center.x, (int) text_center.y, 23);
         
         button_rect.y += button_rect.height + button_padding;
         text_center.y = button_rect.y + button_rect.height/2.0f;
